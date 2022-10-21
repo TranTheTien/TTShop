6fc16673d2a01b02feb3f9392359f097cb916d13
TranTheTien <107165467+TranTheTien@users.noreply.github.com>
2022-10-21 09:17:23 +0700

    Update db.js

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
 
-const url = "mongodb+srv://thetien:TranTheTien1908@cluster0.vvkbikr.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://thetien:TrantheTien1908@cluster0.vvkbikr.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log("MongoDB Connection Succeeded"); } else {
         console.log("An Error Occured");
